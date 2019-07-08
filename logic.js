9fea9bf7e99ee4eeddd9481c1f11cd75167ecae2
Cheska <cheska.gatus@gmail.com>
2019-07-07 23:56:22 -0400

    Edit javascript

--- logic.js
+++ logic.js
@@ -71,7 +71,7 @@ function createMap(earthquakes) {
   var outdoorsmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "mapbox.dark",
+    id: "mapbox.outdoors",
     accessToken: API_KEY
   });
   
